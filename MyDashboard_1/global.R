d1a5f7ebd2b4696bc0d48da5e490811c4dc064a8
igor szymczak <igorszymczak@icloud.com>
2025-05-24 18:47:31 +0200

    updating global.R

--- MyDashboard_1/global.R
+++ MyDashboard_1/global.R
@@ -9,7 +9,13 @@ library(dplyr)
 
 source("plots/explore_plot3.R")
 source("plots/overview_plot2.R")
-color_scale_1 <- c("rgb(68,206,27)", "rgb(187,219,68)", "rgb(247,227,121)", 
-                   "rgb(242,161,52)", "rgb(229,31,31)")
-wine_data <- read.csv("data/grouped_wine_df_nona.csv")
+source("plots/comparison_plots.R")
+
+color_scale_1 <- c("rgb(202, 240, 248)", "rgb(173, 232, 244)", "rgb(144, 224, 239)", 
+                   "rgb(72, 202, 228)", "rgb(0, 180, 216)", "rgb(0, 150, 199)", 
+                  "rgb(0, 119, 182)")
+wine_data <- read.csv("data/wine_data_emb_smaller.csv")
+grouped_wine_data <- read.csv("data/grouped_wine_df_nona.csv")
+reviewers_data <- read.csv("data/reviewers_aggregated_data.csv")
+reviewers <- c(colnames(reviewers_data) , c("None"))
 force_graph <- jsonlite::fromJSON("data/force_graph.json")
